87bb1fab62d89b96610bafd78c5aeafe2ea5b710
Roberto Chauderlot Marchamalo <rchauderlot@gmail.com>
2016-01-28 21:14:23 +0100

    Added some classes scafolds to complete the model

---
+++ src/es/chauder/circuitanalyzer/model/model/base/ParametrizableDevice.java
@@ -0,0 +1,9 @@
+package es.chauder.circuitanalyzer.model.model.base;
+
+/**
+ * Created by rchauderlot on 21/1/16.
+ */
+public abstract class ParametrizableDevice extends Device {
+
+
+}
---
+++ src/es/chauder/circuitanalyzer/model/model/guitar/Wiring.java
@@ -0,0 +1,9 @@
+package es.chauder.circuitanalyzer.model.model.guitar;
+
+/**
+ * Created by rchauderlot on 21/1/16.
+ */
+public class Wiring {
+
+
+}
